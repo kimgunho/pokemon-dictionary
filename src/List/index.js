c6a30a55f587b8a714ffb43d02c5740f7f5131f9
gunho kim <>
2021-10-20 17:59:49 +0900

    포켓몬 fetch후 데이터=> 리스트화

--- src/List/index.js
+++ src/List/index.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './search'
 import Types from './types'
 import Pokemons from './pokemons'
 
 const list = () => {
+  const [pokemons, SetPokemons] = useState([])
+
+  const fetchPokemons = async () => {
+    const res = await fetch(
+      //
+      `https://pokeapi.co/api/v2/pokemon?limit=9&offset=200`,
+    )
+    const json = await res.json()
+    const { results } = json
+
+    const items = await Promise.all(
+      results.map(async ({ url }) => {
+        const res = await fetch(url)
+        const json = await res.json()
+
+        return {
+          id: json.id,
+          name: json.name,
+          img: json.sprites.other.dream_world.front_default,
+          type: json.types[0].type.name,
+        }
+      }),
+    )
+
+    SetPokemons(() => items)
+  }
+
+  useEffect(() => fetchPokemons(), [])
+
   return (
     <div className="items">
       <Search />
       <Types />
-      <Pokemons />
+      <Pokemons items={pokemons} />
     </div>
   )
 }
--- src/List/pokemons.js
+++ src/List/pokemons.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const pokemons = () => {
-  return <>pokemons</>
+const pokemons = ({ items }) => {
+  return (
+    <>
+      <ul className="pokemons">
+        {items.map(({ id, name, img, type }) => (
+          <li key={id} className={type}>
+            <img src={img} alt={name} />
+            <h3>{name}</h3>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default pokemons
