f8cf069c02aebe8505fecf906c29cf3394f8eca3
gunho kim <>
2021-10-20 16:59:39 +0900

    컴포넌트 구성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Detail from './View/Detail'
+import List from './List'
 import './css/style.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Detail />
+      <List />
+    </div>
+  )
 }
 
 export default App
